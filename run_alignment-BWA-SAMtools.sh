1115f8b51bc8fe65fdacf9bcb3a03f2f5f5add2d
wookeemin <98404696+wookeemin@users.noreply.github.com>
2025-07-08 15:14:39 +0200

    Update run_alignment-BWA-SAMtools.sh

--- run_alignment-BWA-SAMtools.sh
+++ run_alignment-BWA-SAMtools.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# 사용자 설정
+# User definition
 THREADS=32
-SAMPLE="sample"  # 파일명 접두어
-REF="ref.fa"     # 기준 유전체
+SAMPLE="sample"  # prefix of file name
+REF="ref.fa"     # Reference genomics
 FASTQ1="${SAMPLE}_1.fastq.gz"
 FASTQ2="${SAMPLE}_2.fastq.gz"
 OUTPUT="aligned.sorted.bam"
 
-# 1. 정렬 수행
+# 1. execution of alignment
 echo "[INFO] Running BWA alignment..."
 bwa mem -t ${THREADS} ${REF} ${FASTQ1} ${FASTQ2} | \
   samtools view -@ 8 -Sb - > aligned.bam
 
-# 2. 정렬 및 인덱싱
+# 2. Alig & Indexing
 echo "[INFO] Sorting BAM file..."
 samtools sort -@ 8 aligned.bam -o ${OUTPUT}
 
 echo "[INFO] Indexing BAM file..."
 samtools index ${OUTPUT}
-#3. 중간 파일 제거 (선택)
+#3. intermediate file remove (optional)
 rm aligned.bam
 
-# 4. 요약 통계 출력
+# 4. Print summary output
 echo "[INFO] Alignment stats:"
 samtools flagstat ${OUTPUT}
